36da8a1f679cb4dca2b8c97272d3658058c21818
Jayden Thompson <jaydenthompson08@protonmail.ch>
2025-10-24 15:19:08 +0100

    added builder for location_type_options

--- janus/ORM/initialize.py
+++ janus/ORM/initialize.py
@@ -1,6 +1,8 @@
 from pony.orm import *
 from database import db
 from entities import *
+from build_enums import build_all_enums
 
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 db.generate_mapping(create_tables=True)
+build_all_enums()
